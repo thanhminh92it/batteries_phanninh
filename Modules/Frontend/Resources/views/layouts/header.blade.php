ee8d75e286f98f34dc92b5100167eec13d6e1585
MinhPT <minhpt@crystal-techs.com>
2017-11-08 17:57:54 +0700

    fix header

--- Modules/Frontend/Resources/views/layouts/header.blade.php
+++ Modules/Frontend/Resources/views/layouts/header.blade.php
@@ -1,26 +1,28 @@
-<div class="container">
-    <div class="pre-header row">
-        <div class="widget-left col-md-6 col-sm-12">
-            Fast Order Shipping, Friendly Service, & the Best Value in Batteries!
-        </div>
-        <div class="widget-right pull-right col-md-6 col-sm-12">
-            <ul class="text-right reset-css">
-                <li>
-                    <a href="#">
-                        Deals & Specials
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Account
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Order History
-                    </a>
-                </li>
-            </ul>
+<div class="pre-header hidden-sm hidden-xs">
+    <div class="container">
+        <div class="row">
+            <div class="widget-left col-md-6 col-sm-12">
+                Fast Order Shipping, Friendly Service, & the Best Value in Batteries!
+            </div>
+            <div class="widget-right pull-right col-md-6 col-sm-12">
+                <ul class="text-right reset-css">
+                    <li>
+                        <a href="#">
+                            Deals & Specials
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Account
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Order History
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
